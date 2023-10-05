3b69476f26d76ae902dd503188ad3cdd156490c8
Lux Le <luclt399471@gmail.com>
2023-10-05 10:23:43 +0700

    Update EsimPlugin.java

--- src/android/EsimPlugin.java
+++ src/android/EsimPlugin.java
@@ -113,7 +113,7 @@ public class EsimPlugin extends CordovaPlugin {
                     PendingIntent.FLAG_MUTABLE);
         
             mgr.downloadSubscription(sub, true, callbackIntent);            
-            callbackContext.sendPluginResult(new PluginResult(Status.OK));
+            callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
         }catch (Exception e) {
             LOG.e(LOG_TAG, "Error install eSIM "  + e.getMessage());
             callbackContext.error("Error install eSIM "  + e.getMessage());
