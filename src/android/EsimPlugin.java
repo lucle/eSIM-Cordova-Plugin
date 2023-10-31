4231ea0e11730e7e8f6d13b0a3973d2835b45e30
Lux Le <luclt399471@gmail.com>
2023-10-31 15:43:53 +0700

    Update EsimPlugin.java

--- src/android/EsimPlugin.java
+++ src/android/EsimPlugin.java
@@ -89,10 +89,10 @@ public class EsimPlugin extends CordovaPlugin{
                     
                     int resultCode = getResultCode();
                     int detailedCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, 0);
-                    int operationCode = intent.getIntExtra(EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE,-1);
-                    int errorCode = intent.getIntExtra(EXTRA_EMBEDDED_SUBSCRIPTION_ERROR_CODE,-1);
-                    String smdxSubjectCode = intent.getStringExtra(EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE);
-                    String smdxReasonCode = intent.getStringExtra(EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE);
+                    int operationCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE,-1);
+                    int errorCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_ERROR_CODE,-1);
+                    String smdxSubjectCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE);
+                    String smdxReasonCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE);
 
                     if (operationCode == EuiccManager.OPERATION_DOWNLOAD && errorCode == EuiccManager.ERROR_CARRIER_LOCKED) {
                             // handle specific error i.e tries to download but the device is carrier locked
