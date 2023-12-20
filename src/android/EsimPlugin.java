1ff240f43cac73c2789f6c5a0216ac83887aa5f4
Lux Le <luclt399471@gmail.com>
2023-12-20 12:16:16 +0700

    Update EsimPlugin.java

--- src/android/EsimPlugin.java
+++ src/android/EsimPlugin.java
@@ -122,7 +122,7 @@ public class EsimPlugin extends CordovaPlugin{
             mainContext.registerReceiver(
                 receiver, 
                 new IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION), 
-                LPA_DECLARED_PERMISSION, 
+                null, 
                 null
             );
             
