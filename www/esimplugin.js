abf5da36057891fceb5020f7cdd9eb440ffd0465
Lux Le <luclt399471@gmail.com>
2024-01-09 23:41:04 +0700

    Update esimplugin.js

--- www/esimplugin.js
+++ www/esimplugin.js
@@ -4,5 +4,8 @@ module.exports = {
   },
   installEsim: function(address, matchingID, iccid, confirmationCode, successCallback, errorCallback) {
       cordova.exec(successCallback, errorCallback, 'EsimPlugin', 'installEsim', [address, matchingID, iccid, confirmationCode]);
+  },
+  installEsimNew: function(matchingID, successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'EsimPlugin', 'installEsimNew', [matchingID]);
   }
 };
