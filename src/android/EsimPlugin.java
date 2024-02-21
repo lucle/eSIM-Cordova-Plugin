4418efe15b6ee825e893beb740299a1826955440
wee hong <weehong.ong@temus.com>
2024-02-21 17:57:19 +0800

    new attempt

--- src/android/EsimPlugin.java
+++ src/android/EsimPlugin.java
@@ -22,24 +22,32 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class EsimPlugin extends CordovaPlugin{
-    
+    // CONSTANTS //
+    private static final String DOWNLOAD_ACTION = "download_subscription"
+    private static final String START_RESOLUTION_ACTION = "start_resolution_action"
+    private static final String BROADCAST_PERMISSION = "com.starhub.aduat.torpedo.lpa.permission.BROADCAST";
+
+    private var manager: EuiccManager? = null
+
+    // eSIM Constants //
     private static final String HAS_ESIM_ENABLED = "hasEsimEnabled";
     private static final String INSTALL_ESIM = "installEsim";
 	private static final String INSTALL_ESIM_NEW = "installEsimNew";
-    private static final String ACTION_DOWNLOAD_SUBSCRIPTION = "download_subscription";
-    private Context mainContext;
-    private EuiccManager mgr;
-    
-    private static final String LPA_DECLARED_PERMISSION = "com.starhub.aduat.torpedo.lpa.permission.BROADCAST";
-    String address;
-    String matchingID;
-    String activationCode ;
+    // FUNCTIONS //
 
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        mainContext = this.cordova.getActivity().getApplicationContext();
-     }
-
+    // Initiate Manager
+    private void initMgr() {
+        if (manager == null) {
+            manager = (EuiccManager) mainContext.getSystemService(Context.EUICC_SERVICE);
+        }
+    }
+    // e-Sim compatibility check
+    private void isSupportEsim(CallbackContext callbackContext) {
+        initMgr();
+        boolean result = manager.isEnabled();
+        callbackContext.sendPluginResult(new PluginResult(Status.OK, result));
+    }
+    // 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (HAS_ESIM_ENABLED.equals(action)) {
@@ -53,12 +61,37 @@ public class EsimPlugin extends CordovaPlugin{
         }    
         return true;
     }
+        /**
+     * Register the broadcast receivers
+     */
+    @Override
+    protected void onStart()
+    {
+        super.onStart()
+        registerReceiver(
+            eSimBroadcastReceiver,
+            IntentFilter(DOWNLOAD_ACTION),
+            BROADCAST_PERMISSION,
+            null
+        )
+        registerReceiver(
+            resolutionReceiver,
+            IntentFilter(START_RESOLUTION_ACTION),
+            BROADCAST_PERMISSION,
+            null
+        )
+    };
 
-    private void initMgr() {
-        if (mgr == null) {
-          mgr = (EuiccManager) mainContext.getSystemService(Context.EUICC_SERVICE);
-        }
-    }
+    /**
+     * Un-Register the broadcast receivers
+     */
+    @Override
+    protected void onStop()
+    {
+        super.onStop()
+        unregisterReceiver(eSimBroadcastReceiver)
+        unregisterReceiver(resolutionReceiver)
+    };
 
     private boolean checkCarrierPrivileges() {
         TelephonyManager telephonyManager  = (TelephonyManager) mainContext.getSystemService(Context.TELEPHONY_SERVICE);
@@ -67,107 +100,18 @@ public class EsimPlugin extends CordovaPlugin{
 
     }
 
-    private void hasEsimEnabled(CallbackContext callbackContext) {
-        initMgr();
-        boolean result = mgr.isEnabled();
-        callbackContext.sendPluginResult(new PluginResult(Status.OK, result));
-    }
-    
-
-    private void installEsim(JSONArray args, CallbackContext callbackContext) {         
-        try{
-            initMgr();             
-            
-            // if (!checkCarrierPrivileges()) {
-            //     callbackContext.error("No carrier privileges detected");
-            //     return;
-            // }
-            BroadcastReceiver receiver = new BroadcastReceiver() {
+	private void installEsimNew(JSONArray args, CallbackContext callbackContext)  
+    {
+        try {
+            BroadcastReceiver receiver =
+            new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
-                    if (!ACTION_DOWNLOAD_SUBSCRIPTION.equals(intent.getAction())) {
+                    if (!action.equals(intent.getAction())) {
                         callbackContext.error("Can't setup eSim due to wrong Intent:" + intent.getAction() + " instead of " + ACTION_DOWNLOAD_SUBSCRIPTION); 
                         return;
                     }
-                    boolean hasCarrierPrivileges = checkCarrierPrivileges();
-                    int resultCode = getResultCode();
-                    int detailedCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, 0);
-                    int operationCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE,-1);
-                    int errorCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_ERROR_CODE,-1);
-                    String smdxSubjectCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE);
-                    String smdxReasonCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE);
-                    
-                    if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR && mgr != null) {                      
-                        // Resolvable error, attempt to resolve it by a user action
-                        int resolutionRequestCode = 0;
-                        PendingIntent callbackIntent = PendingIntent.getBroadcast(
-                            getContext(), 
-                            resolutionRequestCode /* requestCode */, 
-                            intent, 
-                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-                        try{
-                            mgr.startResolutionActivity(cordova.getActivity(), resolutionRequestCode /* requestCode */, intent, callbackIntent);
-                        } catch (Exception e) { 
-                            callbackContext.error("Error startResolutionActivity - Can't add an Esim subscription: " + e.getLocalizedMessage() + " detailedCode=" + detailedCode + 
-                                " operationCode=" + operationCode + " errorCode=" + errorCode + " smdxSubjectCode=" + smdxSubjectCode + " smdxReasonCode=" + smdxReasonCode + " activationCode=" + activationCode + " hasCarrierPrivileges" + hasCarrierPrivileges);
-                            callbackContext.sendPluginResult(new PluginResult(Status.ERROR));     
-                        }                                               
-                    } else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_OK) {  
-                        callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
-                    } else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_ERROR) {
-                        // Embedded Subscription Error     
-                        callbackContext.error("EMBEDDED_SUBSCRIPTION_RESULT_ERROR - Can't add an Esim subscription: detailedCode=" + detailedCode + 
-                                " operationCode=" + operationCode + " errorCode=" + errorCode + " smdxSubjectCode=" + smdxSubjectCode + " smdxReasonCode=" + smdxReasonCode + " activationCode=" + activationCode + " hasCarrierPrivileges" + hasCarrierPrivileges);
-                    } else { 
-                        callbackContext.error("Can't add an Esim subscription due to unknown error, resultCode is:" + String.valueOf(resultCode) + " hasCarrierPrivileges" + hasCarrierPrivileges);
-                    }
-                }
-            };
-            mainContext.registerReceiver(
-                receiver, 
-                new IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION), 
-                LPA_DECLARED_PERMISSION /* broadcastPermission*/, 
-                null /* handler */
-            );
-            
-            address = args.getString(0);
-            matchingID = ReFormatString(args.getString(1), 4);
-            activationCode = "1$" + address + "$" + matchingID;
 
-            // Download subscription asynchronously.
-            DownloadableSubscription sub = DownloadableSubscription.forActivationCode(activationCode /* encodedActivationCode*/);
-            PendingIntent callbackIntent = PendingIntent.getBroadcast(
-                mainContext,
-                0 /* requestCode */,
-                new Intent(ACTION_DOWNLOAD_SUBSCRIPTION),
-                PendingIntent.FLAG_UPDATE_CURRENT |
-                    PendingIntent.FLAG_MUTABLE);
-        
-            mgr.downloadSubscription(sub, true /* switchAfterDownload */, callbackIntent);  
-            //callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
-        }catch (Exception e) {
-            callbackContext.error("Error install eSIM "  + e.getMessage());
-            callbackContext.sendPluginResult(new PluginResult(Status.ERROR));
-        }
-    }
-	
-	private void installEsimNew(JSONArray args, CallbackContext callbackContext) {         
-        try{
-            initMgr();             
-            
-            // if (!checkCarrierPrivileges()) {
-            //     callbackContext.error("No carrier privileges detected");
-            //     return;
-            // }
-
-            BroadcastReceiver receiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    if (!ACTION_DOWNLOAD_SUBSCRIPTION.equals(intent.getAction())) {
-                        callbackContext.error("Can't setup eSim due to wrong Intent:" + intent.getAction() + " instead of " + ACTION_DOWNLOAD_SUBSCRIPTION); 
-                        return;
-                    }
-                    boolean hasCarrierPrivileges = checkCarrierPrivileges();
                     int resultCode = getResultCode();
                     int detailedCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, 0);
                     int operationCode = intent.getIntExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_OPERATION_CODE,0);
@@ -175,166 +119,56 @@ public class EsimPlugin extends CordovaPlugin{
                     String smdxSubjectCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_SUBJECT_CODE);
                     String smdxReasonCode = intent.getStringExtra(EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_SMDX_REASON_CODE);
                     String packageName = mainContext.getPackageName();
+                    boolean hasCarrierPrivileges = checkCarrierPrivileges();
 
-                    if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR && mgr != null) {                      
-                        // Resolvable error, attempt to resolve it by a user action
-                        int resolutionRequestCode = 0;
+                    // If the result code is a resolvable error, call startResolutionActivity
+                    if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR) {
                         PendingIntent callbackIntent = PendingIntent.getBroadcast(
-                            mainContext, 
-                            resolutionRequestCode /* requestCode */, 
-                            intent, 
+                            getContext(), 0 /* requestCode */, intent,
                             PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
-                        try{
-                            mgr.startResolutionActivity(cordova.getActivity(), resolutionRequestCode /* requestCode */, intent, callbackIntent);
-                        } catch (Exception e) { 
+                        try {
+                            manager.startResolutionActivity(
+                            activity,
+                            0 /* requestCode */,
+                            intent,
+                            callbackIntent);
+                        }
+                        catch (Exception e) { 
                             callbackContext.error("Error startResolutionActivity - Can't add an Esim subscription: " + e.getLocalizedMessage() + " detailedCode=" + detailedCode + 
                                 " operationCode=" + operationCode + " errorCode=" + errorCode + " smdxSubjectCode=" + smdxSubjectCode + " smdxReasonCode=" + smdxReasonCode + " activationCode=" + activationCode + 
                                 " hasCarrierPrivileges:" + hasCarrierPrivileges + " package Name = " + packageName);
                             callbackContext.sendPluginResult(new PluginResult(Status.ERROR));     
-                        }                                               
-                    } else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_OK) {  
-                        callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
-                    } else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_ERROR) {
+                        }
+
+                    }
+                    else if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_ERROR) {
                         // Embedded Subscription Error     
                         callbackContext.error("Error Embedded Subscription Error - Can't add an Esim subscription: " + " detailedCode=" + detailedCode + 
                         " operationCode=" + operationCode + " errorCode=" + errorCode + " smdxSubjectCode=" + smdxSubjectCode + " smdxReasonCode=" + smdxReasonCode + " activationCode=" + activationCode + 
                         " hasCarrierPrivileges:" + hasCarrierPrivileges + " package Name = " + packageName);
                         callbackContext.error("Can't add an Esim subscription due to unknown error, resultCode is:" + String.valueOf(resultCode) + " hasCarrierPrivileges" + hasCarrierPrivileges);
                     }
+
+                    resultIntent = intent;
                 }
             };
-            mainContext.registerReceiver(
-                receiver, 
-                new IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION), 
-                LPA_DECLARED_PERMISSION /* broadcastPermission*/, 
-                null /* handler */
-            );
-            
-            activationCode = args.getString(0);
-
+            context.registerReceiver(receiver,
+                    new IntentFilter(DOWNLOAD_ACTION),
+                    BROADCAST_PERMISSION /* broadcastPermission*/,
+                    null /* handler */);
             // Download subscription asynchronously.
-            DownloadableSubscription sub = DownloadableSubscription.forActivationCode(activationCode /* encodedActivationCode*/);
-            Intent intent = new Intent(ACTION_DOWNLOAD_SUBSCRIPTION).setPackage(mainContext.getPackageName());
+            DownloadableSubscription sub = DownloadableSubscription
+            .forActivationCode(code /* encodedActivationCode*/);
+            Intent intent = new Intent(action).setPackage(context.getPackageName());
             PendingIntent callbackIntent = PendingIntent.getBroadcast(
-                mainContext,
-                0 /* requestCode */,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT |
-                    PendingIntent.FLAG_MUTABLE);
-        
-            mgr.downloadSubscription(sub, true /* switchAfterDownload */, callbackIntent);  
-            //callbackContext.sendPluginResult(new PluginResult(Status.OK, true));
+            getContext(), 0 /* requestCode */, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+            manager.downloadSubscription(sub, true /* switchAfterDownload */,
+            callbackIntent);
         }catch (Exception e) {
             callbackContext.error("Error install eSIM "  + e.getMessage());
             callbackContext.sendPluginResult(new PluginResult(Status.ERROR));
         }
     }
-    
-    public String ReFormatString(String S, int K){
-        int len = S.length();
-        int cnt = 0;
-        int x = 0;
-    
-        // Move the characters to the
-        // back of the string.
-        for (int i = len - 1; i >= 0; i--) {
-        if (S.charAt(i) == '-') {
-            x++;
-        }
-        else {
-            S = S.substring(0, i + x)
-            + Character.toUpperCase(S.charAt(i))
-            + S.substring(i + x + 1);
-        }
-        }
-    
-        // Calculate total number of
-        // alphanumeric characters so
-        // as to get the number of dashes
-        // in the final string.
-        int slen = len - x;
-        int step = (int)(slen / K);
-    
-        // Remove x characters from the
-        // start of the string
-    
-        S = reverseS(S);
-        int val = x;
-        while (val > 0) {
-        S = S.substring(0, S.length() - 1);
-        val--;
-        }
-    
-        // Push the empty spaces in
-        // the string (slen+step) to get
-        // the final string length
-    
-        int temp = step;
-        while (temp > 0) {
-        S += " ";
-        temp--;
-        }
-        S = reverseS(S);
-    
-        len = S.length();
-    
-        // Using simple mathematics
-        // to push the elements
-        // in the string at the correct place.
-    
-        int i = slen, j = step, f = 0;
-        while (j < len) {
-    
-        // At every step calculate the
-        // number of dashes that would be
-        // present before the character
-        step = (int)(i / K);
-        if (f == 1)
-            step--;
-        int rem = i % K;
-    
-        // If the remainder is zero it
-        // implies that the character is a dash.
-    
-        if (rem == 0 && f == 0) {
-            S = S.substring(0, j - step) + "-"
-            + S.substring(j - step + 1);
-            f = 1;
-            continue;
-        }
-        S = S.substring(0, j - step) + S.charAt(j)
-            + S.substring(j - step + 1);
-        i--;
-        j++;
-        f = 0;
-        }
-        // Remove all the dashes that would have
-        // accumulated in the beginning of the string.
-    
-        len = S.length();
-        S = reverseS(S);
-        for (int m = len - 1; m >= 0; m--) {
-        if (S.charAt(m) != '-') {
-            break;
-        }
-        if (S.charAt(m) == '-')
-            S = S.substring(0, S.length() - 1);
-        }
-        S = reverseS(S);
-    
-        return S;
-    }
-  
-    public String reverseS(String str)
-    {
-        String nstr = "";
-        for (int i = 0; i < str.length(); i++) {
-            char ch
-            = str.charAt(i); // extracts each character
-            nstr
-            = ch + nstr; // adds each character in
-            // front of the existing string
-        }
-        return nstr;
-    }
+
 }
